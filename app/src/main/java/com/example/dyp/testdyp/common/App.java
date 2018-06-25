74024a2a146d056a31d187903c87b121a0f07929
dyp <526642111@qq.com>
2018-06-25 09:43:31 +0800

    增加 DbFlow加密存储功能

--- app/src/main/java/com/example/dyp/testdyp/common/App.java
+++ app/src/main/java/com/example/dyp/testdyp/common/App.java
@@ -2,10 +2,17 @@ package com.example.dyp.testdyp.common;
 
 import android.app.Application;
 
+import com.example.dyp.testdyp.dbflow.SQLCipherHelperImpl;
+import com.example.dyp.testdyp.dbflow.TestDatabase;
 import com.example.dyp.testdyp.utils.WifiUtil;
 import com.orhanobut.logger.AndroidLogAdapter;
 import com.orhanobut.logger.Logger;
+import com.raizlabs.android.dbflow.config.DatabaseConfig;
+import com.raizlabs.android.dbflow.config.DatabaseDefinition;
+import com.raizlabs.android.dbflow.config.FlowConfig;
 import com.raizlabs.android.dbflow.config.FlowManager;
+import com.raizlabs.android.dbflow.structure.database.DatabaseHelperListener;
+import com.raizlabs.android.dbflow.structure.database.OpenHelper;
 
 public class App extends Application {
     private static App app;
@@ -20,6 +27,16 @@ public class App extends Application {
         app = this;
         Logger.addLogAdapter(new AndroidLogAdapter());
         WifiUtil.getInstance().init(this);
-        FlowManager.init(this);
+        FlowConfig flowConfig = FlowConfig.builder(this)
+                .addDatabaseConfig(new DatabaseConfig.Builder(TestDatabase.class)
+                        .openHelper(new DatabaseConfig.OpenHelperCreator() {
+                            @Override
+                            public OpenHelper createHelper(DatabaseDefinition databaseDefinition, DatabaseHelperListener helperListener) {
+                                return new SQLCipherHelperImpl(databaseDefinition, helperListener);
+                            }
+                        })
+                        .build())
+                .build();
+        FlowManager.init(flowConfig);
     }
 }
--- app/src/main/java/com/example/dyp/testdyp/dbflow/DbFlowActivity.java
+++ app/src/main/java/com/example/dyp/testdyp/dbflow/DbFlowActivity.java
@@ -3,6 +3,7 @@ package com.example.dyp.testdyp.dbflow;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -11,11 +12,19 @@ import android.widget.ListView;
 
 import com.example.dyp.testdyp.R;
 import com.example.dyp.testdyp.base.BaseActivity;
+import com.example.dyp.testdyp.utils.LogUtil;
 
 import java.util.ArrayList;
 import java.util.List;
 
 import butterknife.BindView;
+import io.reactivex.Observable;
+import io.reactivex.ObservableEmitter;
+import io.reactivex.ObservableOnSubscribe;
+import io.reactivex.Observer;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.disposables.Disposable;
+import io.reactivex.schedulers.Schedulers;
 
 public class DbFlowActivity extends BaseActivity {
 
@@ -27,7 +36,9 @@ public class DbFlowActivity extends BaseActivity {
     private String[] controls = new String[]{
             "增加一个设备",
             "删除一个设备",
-            "删除全部设备"
+            "删除全部设备",
+            "增加100个设备",
+            "删除100个设备"
     };
 
     private List<Device> deviceList = new ArrayList<>();
@@ -66,12 +77,96 @@ public class DbFlowActivity extends BaseActivity {
                 switch (position) {
                     case 0: //增加设备
                         deviceAdd();
+                        queryDeviceList();
                         break;
                     case 1: //删除一个设备
-                        deviceDelete();
+                        deviceDelete(1);
+                        queryDeviceList();
                         break;
                     case 2:
                         deviceAllDelete();
+                        queryDeviceList();
+                        break;
+                    case 3:
+                        showLoading();
+                        final long start = System.currentTimeMillis();
+                        LogUtil.i("开始添加: " + start);
+                        Observable.create(new ObservableOnSubscribe<Object>() {
+                            @Override
+                            public void subscribe(ObservableEmitter<Object> e) throws Exception {
+                                for (int i = 0; i < 100; i++) {
+                                    deviceAdd();
+                                }
+                                e.onNext("ok");
+                                e.onComplete();
+                            }
+                        })
+                                .subscribeOn(Schedulers.io())
+                                .observeOn(AndroidSchedulers.mainThread())
+                                .subscribe(new Observer<Object>() {
+                                    @Override
+                                    public void onSubscribe(Disposable d) {
+
+                                    }
+
+                                    @Override
+                                    public void onNext(Object o) {
+                                        LogUtil.i("添加结束: " + System.currentTimeMillis());
+                                        LogUtil.i("耗时: " + (System.currentTimeMillis() - start));
+                                        queryDeviceList();
+                                        hideLoading();
+                                    }
+
+                                    @Override
+                                    public void onError(Throwable e) {
+                                        e.printStackTrace();
+                                        hideLoading();
+                                    }
+
+                                    @Override
+                                    public void onComplete() {
+
+                                    }
+                                });
+                        break;
+                    case 4:
+                        showLoading();
+                        final long start2 = System.currentTimeMillis();
+                        LogUtil.i("开始删除: " + start2);
+                        Observable.create(new ObservableOnSubscribe<Object>() {
+                            @Override
+                            public void subscribe(ObservableEmitter<Object> e) throws Exception {
+                                deviceDelete(100);
+                                e.onNext("ok");
+                                e.onComplete();
+                            }
+                        })
+                                .subscribeOn(Schedulers.io())
+                                .observeOn(AndroidSchedulers.mainThread())
+                                .subscribe(new Observer<Object>() {
+                                    @Override
+                                    public void onSubscribe(Disposable d) {
+
+                                    }
+
+                                    @Override
+                                    public void onNext(Object o) {
+                                        LogUtil.i("添加删除: " + System.currentTimeMillis());
+                                        LogUtil.i("耗时: " + (System.currentTimeMillis() - start2));
+                                        queryDeviceList();
+                                        hideLoading();
+                                    }
+
+                                    @Override
+                                    public void onError(Throwable e) {
+
+                                    }
+
+                                    @Override
+                                    public void onComplete() {
+
+                                    }
+                                });
                         break;
                 }
             }
@@ -118,19 +213,17 @@ public class DbFlowActivity extends BaseActivity {
         device.setPassword("1234");
         device.setUsername("admin");
         device.save();
-
-        queryDeviceList();
     }
 
-    private void deviceDelete() {
+    private void deviceDelete(int num) {
         if (deviceList.size() == 0) {
             toast("无设备");
             return;
         }
-        //删除第一个设备
-        deviceList.get(0).delete();
-
-        queryDeviceList();
+        int deleteNum = Math.min(num, deviceList.size());
+        for (int i = 0; i < deleteNum; i++) {
+            deviceList.get(i).delete();
+        }
     }
 
     private void deviceAllDelete() {
@@ -139,8 +232,6 @@ public class DbFlowActivity extends BaseActivity {
             return;
         }
         TestDatabase.deleteAllDevice();
-
-        queryDeviceList();
     }
 
     private void deviceModifyName(Device device, String name) {
